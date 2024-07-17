71c85d4abc1f11d173606f58cabc98aa17f6928d
Dev-SK01 <srik3713@gmail.com>
2024-07-17 11:50:27 +0530

    SideBar UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import Login from "./components/login/Login";
-
+import { useData } from "./components/contextAPI/DataProvider";
+import Chat from "./components/chat/Chat";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SideBar from "./components/sidebar/SideBar";
 const App = () => {
+  const [{ user }] = useData();
+
   return (
     <>
-      <Login />
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <BrowserRouter>
+            <SideBar />
+            <Routes>
+              <Route path="/" element={<Chat />} />
+              <Route path="/rooms/:roomId" element={<Chat />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      )}
     </>
   );
 };
---
+++ client/src/components/sidebar/SideBar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./sidebar.css";
+import { Avatar, IconButton } from "@mui/material";
+import { useData } from "../contextAPI/DataProvider";
+import {
+  Chat,
+  DonutLarge,
+  MoreVert,
+  SearchOutlined,
+} from "@mui/icons-material";
+
+const SideBar = () => {
+  const [{ user }] = useData();
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar src={user.photoURL} />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLarge />
+          </IconButton>
+          <IconButton>
+            <Chat />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input type="text" placeholder="Search Chat or Start New Chat" />
+        </div>
+      </div>
+      <div className="sidebar__chats">
+
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
